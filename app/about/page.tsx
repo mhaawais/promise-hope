1f1ca739c55f41bf0a725e831364fb5d3568db6f
mhaawais <awais8640107@gmail.com>
2025-10-30 00:21:44 +0500

    2nd tester

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,12 @@
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
 import React from 'react'
 
 const About = () => {
   return (
     <div>
-      About Page
+      <Header />
+      <Footer /> 
     </div>
   )
 }
--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,9 +1,12 @@
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
 import React from 'react';
 
 const Book = () => {
   return (
     <div>
-      Book page
+      <Header />
+      <Footer />
     </div>
   )
 }
